86c346b250b9c0c8e4f1a386c3f2bed533817cdb
nbaramkiazar <naseembaramkiazar@gmail.com>
2020-03-25 13:01:31 -0700

    Lesson 3B

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 30;
+    private PlayerController playerController;
+    private float leftBoundary = -15;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed); 
+        if (playerController.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (transform.position.x < leftBoundary && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public GameObject bgCratePrefab;
     private Vector3 spawnLocation = new Vector3(30, 0, 0);
     private float initalWait = 2;
     private float periodRepeat = 2;
+    private PlayerController playerController;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", initalWait, periodRepeat);
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        int bgBoxCount = 20;
+        float bgWidth = GameObject.Find("Background").GetComponent<BoxCollider>().size.x;
+        float spacing = bgWidth / bgBoxCount;
+
+        for (int count = 0; count < bgBoxCount; count++)
+        {
+            Debug.Log(count * spacing);
+            Instantiate(bgCratePrefab, new Vector3(count * spacing, 0, 4.15f), bgCratePrefab.transform.rotation);
+        }
+    }
+
+    void Update()
+    {
+
     }
 
     // Update is called once per frame
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
+        if (playerController.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
+        }
     }
 }
